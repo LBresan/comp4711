28de188b0795888aafbe665a5cd9a5293a2680a1
Luka <i-am-luka@hotmail.com>
2015-09-10 19:50:44 -0700

    Added students, documentation, ...

--- index.php
+++ index.php
@@ -11,7 +11,59 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hello out there... I lived";
+        include('student.php');
+
+        //an array to hold students.
+        $students = array();
+        
+        //student object
+        $first = new student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        
+        //adds the first student to the array of students.
+        $students['j123'] = $first;
+        
+        //student object
+        $second = new student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        
+        //adds the second student to the array of students.
+        $students['a456'] = $second;
+        
+        //student object
+        $third = new student();
+        $third->surname = "Bresan";
+        $third->first_name = "Luka";
+        $third->add_email('home','luka@maniacs.com');
+        $third->add_email('work','l_bresan@sundran.com');
+        $third->add_email('school','luka@cst.bcit.edu');
+        $third->add_grade(6);
+        $third->add_grade(6);
+        $third->add_grade(6);
+        
+        //adds the third student to the array of students.
+        $students['l789'] = $third;
+        
+        //sorts the list of students by key.
+        ksort($students);
+        
+        //prints each student, and their info, to the screen.
+        foreach ($students as $student) {
+            echo $student->toString();
+        }        
         ?>
     </body>
 </html>
